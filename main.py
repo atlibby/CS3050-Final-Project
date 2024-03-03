82d241f4124297ff4ec7bebd7ce76dfd170bdc48
Nathaniel Borrok-Hoffman <nathaniel.borrok-hoffman@uvm.edu>
2024-03-03 15:09:30 -0500

    dealing with merge conflicts

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
+"""
+Array Backed Grid Shown By Sprites
+
+Show how to use a two-dimensional list/array to back the display of a
+grid on-screen.
+
+This version syncs the grid to the sprite list in one go using resync_grid_with_sprites.
+This is faster than rebuilding a shape list every time the grid changes,
+but we are still inspecting every single cell of the grid when it updates.
+There are faster ways, but this works for smaller grid sizes.
+
+If Python and Arcade are installed, this example can be run from the command line with:
+python -m arcade.examples.array_backed_grid_sprites_1
+"""
 import arcade
-import os
 
 # Set how many rows and columns we will have
 ROW_COUNT = 15
@@ -16,14 +29,36 @@ MARGIN = 5
 # Do the math to figure out our screen dimensions
 SCREEN_WIDTH = (WIDTH + MARGIN) * COLUMN_COUNT + MARGIN
 SCREEN_HEIGHT = (HEIGHT + MARGIN) * ROW_COUNT + MARGIN
-SCREEN_TITLE = "Clue"
+SCREEN_TITLE = "Array Backed Grid Example"
 
 
-class clueGame(arcade.Window):
+class ClueGame(arcade.Window):
+    """
+    Main application class.
+    """
+
     def __init__(self, width, height, title):
-        # Call parent class and set up game window
+        """
+        Set up the application.
+        """
         super().__init__(width, height, title)
 
+        # Create a 2 dimensional array. A two dimensional
+        # array is simply a list of lists.
+        # This array can be altered later to contain 0 or 1
+        # to show a white or green cell.
+        # 
+        # A 4 x 4 grid would look like this
+        #
+        # grid = [
+        #     [0, 0, 0, 0],
+        #     [0, 0, 0, 0],
+        #     [0, 0, 0, 0],
+        #     [0, 0, 0, 0],
+        # ]
+        # We can quickly build a grid with python list comprehension
+        # self.grid = [[0] * COLUMN_COUNT for _ in range(ROW_COUNT)]
+        # Making the grid with loops:
         self.grid = []
         for row in range(ROW_COUNT):
             # Add an empty array that will hold each cell
@@ -32,13 +67,12 @@ class clueGame(arcade.Window):
             for column in range(COLUMN_COUNT):
                 self.grid[row].append(0)  # Append a cell
 
-
-        arcade.set_background_color(arcade.color.AMAZON)
-        self.background = None
-
+        # Set the window's background color
+        self.background_color = arcade.color.BLACK
         # Create a spritelist for batch drawing all the grid sprites
         self.grid_sprite_list = arcade.SpriteList()
 
+        # Create a list of solid-color sprites to represent each grid location
         for row in range(ROW_COUNT):
             for column in range(COLUMN_COUNT):
                 x = column * (WIDTH + MARGIN) + (WIDTH / 2 + MARGIN)
@@ -47,8 +81,7 @@ class clueGame(arcade.Window):
                 sprite.center_x = x
                 sprite.center_y = y
                 self.grid_sprite_list.append(sprite)
-        # file_path = os.path.dirname(os.path.abspath(__file__))
-        # os.chdir(file_path)
+
     def resync_grid_with_sprites(self):
         """
         Update the color of all the sprites to match
@@ -72,11 +105,6 @@ class clueGame(arcade.Window):
                 else:
                     self.grid_sprite_list[pos].color = arcade.color.GREEN
 
-    # Restart game
-    def setup(self):
-        self.background = arcade.load_texture("clue_board2.jpg")
-
-    # Draw screen
     def on_draw(self):
         """
         Render the screen.
@@ -87,10 +115,35 @@ class clueGame(arcade.Window):
         # Batch draw all the sprites
         self.grid_sprite_list.draw()
 
+    def on_mouse_press(self, x, y, button, modifiers):
+        """
+        Called when the user presses a mouse button.
+        """
+
+        # Convert the clicked mouse position into grid coordinates
+        column = int(x // (WIDTH + MARGIN))
+        row = int(y // (HEIGHT + MARGIN))
+
+        print(f"Click coordinates: ({x}, {y}). Grid coordinates: ({row}, {column})")
+
+        # Make sure we are on-grid. It is possible to click in the upper right
+        # corner in the margin and go to a grid location that doesn't exist
+        if row >= ROW_COUNT or column >= COLUMN_COUNT:
+            # Simply return from this method since nothing needs updating
+            return
+
+        # Flip the location between 1 and 0.
+        if self.grid[row][column] == 0:
+            self.grid[row][column] = 1
+        else:
+            self.grid[row][column] = 0
+
+        # Update the sprite colors to match the new grid
+        self.resync_grid_with_sprites()
+
 
 def main():
-    screen = clueGame(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
-    screen.setup()
+    ClueGame(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
     arcade.run()
 
 
