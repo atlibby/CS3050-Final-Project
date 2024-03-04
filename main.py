479cc6e94132fbb74ca2ca943080d68300622c66
r3ub <saxophone1539@gmai.com>
2024-03-04 16:33:07 -0500

    room class and generate_rooms

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import arcade
+import random
+from room import Room
 
 SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 650
@@ -20,6 +22,27 @@ class clueGame(arcade.Window):
         self.clear()
 
 
+#simulates a roll of the dice using Clue die faces
+def dice_roll():
+    roll_options = ['c', 2, 3, 4, 5, 6]
+    return random.choice(roll_options)
+
+#returns a list of the classic rooms from Clue
+def generate_rooms():
+    #Hall, Lounge, Dining Room, Kitchen, Ballroom, Conservatory, Billiard Room, Library, and Study
+    hall = Room("hall", "", [1, 1])
+    lounge = Room("lounge", "conservatory", [1, 1])
+    dining_room = Room("dining_room", "", [1, 1])
+    kitchen = Room("kitchen", "study", [1, 1])
+    ballroom = Room("ballroom", "", [1, 1])
+    conservatory = Room("conservatory", "lounge", [1, 1])
+    billiard_room = Room("billiard_room", "", [1, 1])
+    library = Room("library", "", [1, 1])
+    study = Room("study", "kitchen", [1, 1])
+    return [hall, lounge, dining_room, kitchen, ballroom, conservatory, billiard_room, library, study]
+
+
+
 def main():
     screen = clueGame()
     screen.setup()
@@ -27,4 +50,6 @@ def main():
 
 
 if __name__ == "__main__":
+    print(dice_roll())
+    rooms = generate_rooms()
     main()
