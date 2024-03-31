961689e0b7123514f7386d36526bbd2fc30a5a59
Nathaniel Borrok-Hoffman <nathaniel.borrok-hoffman@uvm.edu>
2024-03-31 19:35:30 -0400

    added inventory title

--- game_screens.py
+++ game_screens.py
@@ -1,5 +1,20 @@
 import arcade
+# Set how many rows and columns we will have
+ROW_COUNT = 24
+COLUMN_COUNT = 34
 
+TIME = 0.5
+
+# This sets the WIDTH and HEIGHT of each grid location
+WIDTH = 30
+HEIGHT = 30
+
+# This sets the margin between each cell and offset for screen edges
+MARGIN = 2
+
+# Screen dimensions
+SCREEN_WIDTH = (WIDTH + MARGIN) * COLUMN_COUNT + MARGIN
+SCREEN_HEIGHT = (HEIGHT + MARGIN) * ROW_COUNT + MARGIN
 # show the inventory of the player
 class InventoryMenu(arcade.View):
     def __init__(self, game_view, hand):
@@ -15,6 +30,7 @@ class InventoryMenu(arcade.View):
 
     def on_draw(self):
         arcade.start_render()
+        arcade.draw_text("Inventory", SCREEN_WIDTH/2, SCREEN_HEIGHT-30, arcade.color.WHITE, 24)
 
     def on_key_press(self, key, modifiers):
          self.window.show_view(self.game_view)
